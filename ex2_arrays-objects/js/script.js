6dc4767e53735a740d53479c225d496f63ca5be3
Daniel Ferreira <daniel_carra@hotmail.com>
2023-11-30 22:27:47 +0000

    first exercises

---
+++ ex2_arrays-objects/js/script.js
@@ -0,0 +1,11 @@
+function meuEscopo() {
+    const form = document.querySelector('.form');
+
+    // form.onsubmit = function (evento) {
+    //     evento.preventDefault();
+    //     console.log('Foi enviado.');
+    // };
+
+}
+
+meuEscopo();
