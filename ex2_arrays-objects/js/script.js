48746f72bf24bfa98b5f236d6f6cd7dab8b812d4
Daniel Ferreira <daniel_carra@hotmail.com>
2023-12-01 00:02:56 +0000

    exemplo de formulário

--- ex2_arrays-objects/js/script.js
+++ ex2_arrays-objects/js/script.js
@@ -1,11 +1,31 @@
 function meuEscopo() {
     const form = document.querySelector('.form');
+    const pessoas = [];
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
 
     // form.onsubmit = function (evento) {
     //     evento.preventDefault();
     //     console.log('Foi enviado.');
     // };
-
 }
 
 meuEscopo();
