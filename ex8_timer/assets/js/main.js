426d960e8e389023e739950aa74d287b2c9d2d92
Daniel Ferreira <daniel_carra@hotmail.com>
2023-12-21 17:51:50 +0000

    creating a timer - other solution

--- ex8_timer/assets/js/main.js
+++ ex8_timer/assets/js/main.js
@@ -1,24 +1,52 @@
-function  getTimeFromSeconds(seconds) {
-    const data = new Date(seconds * 1000);
-    return data.toLocaleTimeString('pt-PT', {
-        hour12: false,
-        timeZone: 'UTC'
+function timer(){
+    function  getTimeFromSeconds(seconds) {
+        const data = new Date(seconds * 1000);
+        return data.toLocaleTimeString('pt-PT', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+    const relogio = document.querySelector('.relogio');
+    //const iniciar = document.querySelector('.iniciar');
+    //const pausar = document.querySelector('.pausar');
+    //const zerar = document.querySelector('.zerar');
+    let seconds = 0;
+    let timer;
+
+    function startTimer(){
+        timer = setInterval(function(){
+            seconds++;
+            relogio.innerHTML = getTimeFromSeconds(seconds);
+        }, 1000);
+    }
+
+    document.addEventListener('click', function(e){
+        const el = e.target;
+
+        if(el.classList.contains('iniciar')){
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            startTimer();
+        }
+
+        if(el.classList.contains('pausar')){
+            clearInterval(timer);
+            relogio.classList.add('pausado');   
+        }
+
+        if(el.classList.contains('zerar')){
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado');
+            seconds = 0;  
+        }
     });
 }
 
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-let seconds = 0;
-let timer;
+timer();
 
-function startTimer(){
-    timer = setInterval(function(){
-        seconds++;
-        relogio.innerHTML = getTimeFromSeconds(seconds);
-    }, 1000);
-}
+/*
 
 iniciar.addEventListener('click', function(e){
     relogio.classList.remove('pausado');
@@ -38,3 +66,4 @@ zerar.addEventListener('click', function(e){
     seconds = 0;
 });
 
+*/
