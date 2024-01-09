bc96b769aca85257d1cdc1f6dbbcb59beb7ef6a3
Daniel Ferreira <daniel_carra@hotmail.com>
2024-01-09 21:58:01 +0000

    reduce

---
+++ Extras/reduce.js
@@ -0,0 +1,30 @@
+//Somar todos os numeros pares (reduce)
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const total = numeros.reduce(function(acumulador, valor, indice, array) {
+    if(valor % 2 === 0) {
+        acumulador += valor; 
+    }
+
+    return acumulador;
+}, 0);
+
+//console.log(total);
+
+//Retornar a pessoa mais velha
+
+const pessoas = [
+    {nome: 'Luiz', idade: 62}, 
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 105},
+    {nome: 'Letícia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47},
+];
+
+const maisVelha = pessoas.reduce(function(acumulador, valor) {
+    if(acumulador.idade > valor.idade) return acumulador;
+    return valor;
+});
+
+console.log(maisVelha);
