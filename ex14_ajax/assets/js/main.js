bed6a04cdc05c9604a34580e17f64e1af510c678
Daniel Ferreira <daniel_carra@hotmail.com>
2024-02-21 14:33:07 +0000

    fetch API

--- ex14_ajax/assets/js/main.js
+++ ex14_ajax/assets/js/main.js
@@ -2,7 +2,7 @@
 interagir com eles de forma assíncrona. Ele é frequentemente usado para atualizar partes de uma página da web 
 sem a necessidade de recarregá-la completamente.*/
 
-const request = obj => {
+/* const request = obj => {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();                                 // 1 - vamos instanciar um novo objeto XMLHttpRequest e atriubui-lo a variável xhr.
         xhr.open(obj.method, obj.url, true);                              // 2 - utilizamos o método open para configurar a requisição.
@@ -16,7 +16,7 @@ const request = obj => {
             }
         });
     });
-};                
+};   */              
 
 document.addEventListener('click', e => {
     const el = e.target;
@@ -29,20 +29,38 @@ document.addEventListener('click', e => {
 });
 
 async function loadPage(el) {
-    const href = el.getAttribute('href');
-    
-    const objConfig = {                                                     // 5 - aqui vamos definir os valores do objeto request.
-        method: 'GET',
-        url: href
-    };                                                           
-    
     try {
-        const response = await request(objConfig);
-        loadResult(response);
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+
+        if(response.status !== 200) throw new Error('ERRO 404');
+
+        const html = await response.text();
+        loadResult(html);
     } catch(e) {
         console.log(e);
     }
     
+
+    /* const objConfig = {                                                     // 5 - aqui vamos definir os valores do objeto request.
+        method: 'GET',
+        url: href
+    }; */                                                           
+    
+    /* try {
+        const response = await request(objConfig);
+        loadResult(response);
+    } catch(e) {
+        console.log(e);
+    } */
+
+    /* fetch(href)
+        .then(response => {
+            if(response.status !== 200) throw new Error('ERRO 404');
+            return response.text();
+        })
+        .then(html => loadResult(html))
+        .catch(e => console.log(e)); */
 }
 
 function loadResult(response) {
