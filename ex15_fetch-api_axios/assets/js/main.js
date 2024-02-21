97d96b4eb9bd5ae72dac2f6299be6215ec1322d4
Daniel Ferreira <daniel_carra@hotmail.com>
2024-02-21 15:11:07 +0000

    create a table to save data from a json document using fetch

---
+++ ex15_fetch-api_axios/assets/js/main.js
@@ -0,0 +1,28 @@
+fetch('pessoas.json')                                           // 1 - vamos buscar os dados que estão guardados no ficheiro json.
+    .then(response => response.json())                          // 2 - recebemos os dados através da fetch api.
+    .then(json => loadElements(json));                          // 3 - o próprio json retorna uma nova promise.
+
+function loadElements(json) {
+    const table = document.createElement('table');
+    for(let pessoa of json) {
+        const tr = document.createElement('tr');
+
+        let td1 = document.createElement('td');
+        td1.innerHTML = pessoa.nome;
+        tr.appendChild(td1)
+
+        let td2 = document.createElement('td');
+        td2.innerHTML = pessoa.idade;
+        tr.appendChild(td2)
+
+        let td3 = document.createElement('td');
+        td3.innerHTML = pessoa.salario;
+        tr.appendChild(td3)
+
+        table.appendChild(tr);
+    }
+
+    const resultado = document.querySelector('.resultado');
+    resultado.appendChild(table);
+    
+}
