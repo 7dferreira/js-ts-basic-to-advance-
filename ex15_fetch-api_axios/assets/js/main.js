a24d6f186f96a89097ad77de54b7f79eaaa5ccfc
Daniel Ferreira <daniel_carra@hotmail.com>
2024-02-21 15:27:11 +0000

    http request with axios

--- ex15_fetch-api_axios/assets/js/main.js
+++ ex15_fetch-api_axios/assets/js/main.js
@@ -1,10 +1,13 @@
-fetch('pessoas.json')                                           // 1 - vamos buscar os dados que estão guardados no ficheiro json.
-    .then(response => response.json())                          // 2 - recebemos os dados através da fetch api.
-    .then(json => loadElements(json));                          // 3 - o próprio json retorna uma nova promise.
+/* fetch('pessoas.json')                                           // 1 - vamos buscar os dados que estão guardados no ficheiro json.
+    .then(response => response.json())                             // 2 - recebemos os dados através da fetch api.
+    .then(json => loadElements(json));   */                        // 3 - o próprio json retorna uma nova promise.
 
-function loadElements(json) {
+axios('pessoas.json')
+    .then(response => loadElements(response.data));                // 6 - em vez de utilizarmos fetch api, podemos utilizar axios.
+
+function loadElements(json) {                                   // 4 - criamos uma função que vai carregar os elementos na página através de uma tabela.
     const table = document.createElement('table');
-    for(let pessoa of json) {
+    for(let pessoa of json) {                                   // 5 - percorremos o ficheiro json e vamos buscar as informações que pretendemos.
         const tr = document.createElement('tr');
 
         let td1 = document.createElement('td');
@@ -15,14 +18,14 @@ function loadElements(json) {
         td2.innerHTML = pessoa.idade;
         tr.appendChild(td2)
 
-        let td3 = document.createElement('td');
+       /*  let td3 = document.createElement('td');
         td3.innerHTML = pessoa.salario;
-        tr.appendChild(td3)
+        tr.appendChild(td3) */
 
         table.appendChild(tr);
     }
 
     const resultado = document.querySelector('.resultado');
     resultado.appendChild(table);
-    
+
 }
