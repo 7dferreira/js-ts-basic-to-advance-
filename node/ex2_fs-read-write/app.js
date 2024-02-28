ac3585059b77da7177eb512b7da63650d8db7586
Daniel Ferreira <daniel_carra@hotmail.com>
2024-02-28 11:56:37 +0000

    read a file with fs module

--- node/ex2_fs-read-write/app.js
+++ node/ex2_fs-read-write/app.js
@@ -1,13 +1,26 @@
 const path = require("path");
 const filePath = path.resolve(__dirname, "test.json");
 const write = require('./modules/write');
+const read = require('./modules/read');
 
-const people = [
-    { nome: "João" },
-    { nome: "Maria" },
-    { nome: "Alberto" },
-    { nome: "Tiago" },
-  ];
+// const people = [
+//     { nome: "João" },
+//     { nome: "Maria" },
+//     { nome: "Alberto" },
+//     { nome: "Tiago" },
+//   ];
   
-  const json = JSON.stringify(people, '', 2);
-  write(filePath, json);
+//   const json = JSON.stringify(people, '', 2);
+//   write(filePath, json);
+
+async function readFile(filePath) {         // a partir do momento que retornamos de uma função que é assinc, vamos retornar uma promessa.
+  const data = await read(filePath);
+  renderData(data);
+}
+
+function renderData(data){
+  data = JSON.parse(data);
+  data.forEach(val => console.log(val.nome));
+}
+
+readFile(filePath);
--- node/ex2_fs-read-write/modules/read.js
+++ node/ex2_fs-read-write/modules/read.js
@@ -1,5 +1,4 @@
 const fs = require("fs").promises;
 
-module.exports = (filePath, data) => {
-    fs.readFile(filePath, data, { flag: "w" }); 
-};
+module.exports = (filePath) => fs.readFile(filePath, 'utf8'); 
+
