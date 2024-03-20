3b4b66631c344a65538024189db58e5076704b1c
Daniel Ferreira <daniel_carra@hotmail.com>
2024-03-20 14:26:39 +0000

    inject data into views with ejs

--- node/ex3_express/src/controllers/homeController.js
+++ node/ex3_express/src/controllers/homeController.js
@@ -8,7 +8,10 @@
     .catch(e => console.log(e)); */
 
 exports.inicialPage = (req, res) => {   // o cliente faz uma req, o servidor entrega a resp dessa req.   
-    res.render('index');
+    res.render('index', {
+        titulo: 'Este será o titulo da página',
+        numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    });
     return;
 };
 
--- node/ex3_express/src/middlewares/middleware.js
+++ node/ex3_express/src/middlewares/middleware.js
@@ -1,3 +1,4 @@
 exports.globalMiddleware = (req, res, next) => {
+    res.locals.localVar = 'Este é o valor da variável local.';
     next();
 };
