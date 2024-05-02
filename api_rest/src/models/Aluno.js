3b98ad9582da1c27c8c64325c1cc9a992498e5b1
Daniel Ferreira <daniel_carra@hotmail.com>
2024-05-02 19:56:27 +0100

    CRUD students

--- api_rest/src/models/Aluno.js
+++ api_rest/src/models/Aluno.js
@@ -4,12 +4,65 @@ export default class Aluno extends Model {
   static init(sequelize) {
     super.init(
       {
-        nome: Sequelize.STRING,
-        sobrenome: Sequelize.STRING,
-        email: Sequelize.STRING,
-        idade: Sequelize.INTEGER,
-        peso: Sequelize.FLOAT,
-        altura: Sequelize.FLOAT,
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'Nome precisa de ter entre 3 e 255 caracteres.',
+            },
+          },
+        },
+        sobrenome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'Sobrenome precisa de ter entre 3 e 255 caracteres.',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          unique: {
+            msg: 'Este email já existe.',
+          },
+          validate: {
+            isEmail: {
+              msg: 'Email inválido.',
+            },
+          },
+        },
+        idade: {
+          type: Sequelize.INTEGER,
+          defaultValue: '',
+          validate: {
+            isInt: {
+              msg: 'Idade inválida.',
+            },
+          },
+        },
+        peso: {
+          type: Sequelize.FLOAT,
+          defaultValue: '',
+          validate: {
+            isFloat: {
+              msg: 'Peso inválido.',
+            },
+          },
+        },
+        altura: {
+          type: Sequelize.FLOAT,
+          defaultValue: '',
+          validate: {
+            isFloat: {
+              msg: 'Altura inválida.',
+            },
+          },
+        },
       },
       {
         sequelize,
