0161eb6cefbb2090b73348a5d1d2dbcfd6156b43
Daniel Ferreira <daniel_carra@hotmail.com>
2024-05-09 14:22:42 +0100

    save tasks on local storage

--- react/lista_tarefas/src/components/Main.js
+++ react/lista_tarefas/src/components/Main.js
@@ -11,11 +11,29 @@ export default class Main extends Component {
     index: -1,
   };
 
+  componentDidMount() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if (!tasks) return;
+
+    this.setState({ tasks });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tasks } = this.state;
+
+    if (tasks === prevState.tasks) return;
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     const { tasks, index } = this.state;
     let { newTask } = this.state;
     newTask = newTask.trim();
+
+    if (!newTask) return;
 
     if (tasks.indexOf(newTask) !== -1) return; // Verifica se existem tarefas repetidas.
 
