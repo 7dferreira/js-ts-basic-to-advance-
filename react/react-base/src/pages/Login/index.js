532525b7313d53b8b682464a41430bfb5145180c
Daniel Ferreira <daniel_carra@hotmail.com>
2024-06-03 17:09:39 +0100

    react toastify

--- react/react-base/src/pages/Login/index.js
+++ react/react-base/src/pages/Login/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragraph } from './styled';
 
 export default function Login() {
+  toast.success('Conta criada com sucesso!');
+  toast.error('Erro!');
   return (
     <Container>
       <Title>
