f3b689e116eb5c714968b842e8ff27ecfcd5de8f
Daniel Ferreira <daniel_carra@hotmail.com>
2024-06-12 16:51:34 +0100

    redux persist

--- react/react-base/src/store/index.js
+++ react/react-base/src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+
+import persistedReducers from './modules/reduxPersist';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -14,10 +17,14 @@ import rootSaga from './modules/rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  persistedReducers(rootReducer),
+  applyMiddleware(sagaMiddleware)
+);
 
 sagaMiddleware.run(rootSaga);
 
+export const persistor = persistStore(store);
 export default store;
 
 // -> ação -> reducer(listener) -> newState || actualState
---
+++ react/react-base/src/store/modules/reduxPersist.js
@@ -0,0 +1,11 @@
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+
+export default (reducers) => {
+  const persistReducers = persistReducer(
+    { key: 'REACT-BASE', storage, whitelist: ['example'] },
+    reducers
+  );
+
+  return persistReducers;
+};
