eccfec5914e1495d7136ea46d605c68526f359cf
Daniel Ferreira <daniel_carra@hotmail.com>
2024-06-14 15:02:10 +0100

    display students and images

--- react/consume_api/src/pages/Alunos/index.js
+++ react/consume_api/src/pages/Alunos/index.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { get } from 'lodash';
+import { Link } from 'react-router-dom';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
+// serve para exibir alguma coisa no lugar dos alunos q n têm foto.
 
 import { Container } from '../../styles/GlobalStyles';
-import { AlunoContainer } from './styled';
+import { AlunoContainer, ProfilePicture } from './styled';
 import axios from '../../services/axios';
 
 export default function Alunos() {
@@ -20,7 +24,23 @@ export default function Alunos() {
       <AlunoContainer>
         {alunos.map((aluno) => (
           <div key={String(aluno.id)}>
-            <img src={aluno.Images[0].url} alt='' />
+            <ProfilePicture>
+              {get(aluno, 'Images[0].url', false) ? (
+                <img src={aluno.Images[0].url} alt='' />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+
+            <span>{aluno.nome}</span>
+            <span>{aluno.email}</span>
+
+            <Link to={`/aluno/${aluno.id}/edit`}>
+              <FaEdit size={16} />
+            </Link>
+            <Link to={`/aluno/${aluno.id}/delete`}>
+              <FaWindowClose size={16} />
+            </Link>
           </div>
         ))}
       </AlunoContainer>
