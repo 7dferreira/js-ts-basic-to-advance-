187c606b4508b3b664744b79a2065631af406c7d
Daniel Ferreira <daniel_carra@hotmail.com>
2024-07-10 17:50:32 +0100

    registration page

--- react/consume_api/src/pages/Register/index.js
+++ react/consume_api/src/pages/Register/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
 export default function Register() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Container>
-      <h1>Register</h1>
+      <h1>Criar conta</h1>
+
+      <Form>
+        <label htmlFor='nome'>
+          Nome:
+          <input
+            type='text'
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </label>
+        <label htmlFor='email'>
+          Email:
+          <input
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor='password'>
+          Password:
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+      </Form>
     </Container>
   );
 }
--- react/consume_api/src/pages/Register/styled.js
+++ react/consume_api/src/pages/Register/styled.js
@@ -1,3 +1,29 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragraph = styled.p``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+
+  label {
+    display: grid;
+    grid-template-columns: 80px 1fr;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+
+  input {
+    height: 40px;
+    width: 100%;
+    font-size: 18px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+    border-radius: 4px;
+    margin-left: 10px;
+
+    &:focus {
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
+`;
